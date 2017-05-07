2ec527307466ee5677330ebc81f2ab9fa295ea81
James Abney <james.abney@gmail.com>
2017-05-07 00:32:57 -0700

    Build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,7 @@ let common = require('./webpack.common.js')
 
 module.exports = webpackMerge(common.client, {
 
-    devtool: 'cheap-module-source-map',
+    devtool: 'source-map',
 
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
